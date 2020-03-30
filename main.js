6e792a27eef8bb0405ee3e56cda83e4aaec50ac4
xiaoguoguo <1662106538@qq.com>
2020-03-30 22:18:14 +0800

    创建添加音乐窗口

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 
-function createWindow () {   
-  // 创建浏览器窗口
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
+class appWindow extends BrowserWindow {
+  constructor(config, filepath) {
+    const baseConfig = {
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
     }
-  })
+    const finalConfig = { ...baseConfig, ...config }
+    super(finalConfig);
+    this.loadFile(filepath)
+    this.once('ready-to-show', () => {
+      this.show()
+    })
+  }
+}
 
-  // 加载index.html文件
-  win.loadFile('./renderer/index.html')
+function createWindow() {
+  // 创建浏览器窗口
+  let win = new appWindow({}, './renderer/index.html')
+  ipcMain.on('add', () => {
+    let addMusic = new appWindow({
+      width: 600,
+      height: 400,
+      parent: win
+    }, './renderer/add.html')
+  })
 }
 
 app.whenReady().then(createWindow)
---
+++ renderer/index.js
@@ -0,0 +1,4 @@
+const { ipcRenderer } = require('electron')
+document.getElementById('add-music').addEventListener("click",()=>{
+    ipcRenderer.send('add', 'music')
+})
